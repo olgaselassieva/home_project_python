ed7cdd892c4b73dfc2465c32c03e0ac20ada4c51
olga_s <oklahomochka@gmail.com>
2022-03-31 20:57:29 +0300

    tipy dannyh add testfile

--- pythontest.py
+++ pythontest.py
@@ -1,1 +1,21 @@
-print("hello world")
+#soxranyem v peremennuy chislo i vyvedem ee znachenie na ekran
+a = 14
+print(a)
+#analogichno sdelaem s txt
+b = 'text'
+print(b)
+#deistviya i tipy dannyh
+a = 14
+b = 3
+c = a + b
+print(c)
+#mozno li izmenit` znachenie peremennoi?
+a = 18
+print(a)
+e = a
+print(e)
+#tipy dannyh
+#integer
+a = 1
+print(a)
+print(type(a))
