fe5198bd59f0873fe4c069b7d76475a8fd39b673
olga_s <oklahomochka@gmail.com>
2022-03-31 23:19:08 +0300

    input variables request print

--- Homework01_python.py
+++ Homework01_python.py
@@ -3,3 +3,13 @@ a = 1
 b = 30
 c = b - a
 print(c)
+
+
+name = input("enter your username: ")
+
+password = int(input("enter your password(1;10): "))
+print("username is", name,"and his password is", password)
+
+
+
+
