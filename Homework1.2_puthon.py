688980729f72a2d3dec1d738f7275fbd2a05731b
olga_s <oklahomochka@gmail.com>
2022-04-01 20:27:55 +0300

    input seconds

--- Homework1.2_puthon.py
+++ Homework1.2_puthon.py
@@ -1,2 +1,21 @@
-#clock,minutes,seconds
+
+
+a = int(input('input seconds:'))
+while a > 86400:
+    a = int(input('input seconds<86400:'))
+    break
+if a <= 86400:
+      hh = a // 3600
+      b = a % 3600
+      mm = b // 60
+      ss = b % 60
+
+print(hh, ':', mm, ':', ss)
+
+
+
+
+
+
+
 
