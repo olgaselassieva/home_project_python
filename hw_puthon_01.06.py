494a82bc720df8769186c9edd2af5c51cdaf6625
olga_s <oklahomochka@gmail.com>
2022-04-26 00:02:35 +0300

    change hw  01.04

--- hw_puthon_01.06.py
+++ hw_puthon_01.06.py
@@ -7,11 +7,11 @@ while True:
     pp = 0
     if rev > cost:
         p = rev - cost
-        ren = p / rev * 100
+        ren = int(p / rev * 100)
         print('Прибыль равна', p)
         print('Рентабельность равна', ren)
         people = int(input('Кол-во ваших сотрудников: '))
-        pp = p / people
+        pp = p // people
         print('Прибыль на 1 сотрудника равна', pp)
 
     elif rev < cost:
